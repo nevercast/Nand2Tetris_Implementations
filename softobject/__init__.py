5b03c38943568e594efd89f225e748015be94217
Josh Lloyd <j.nevercast@gmail.com>
2016-05-14 17:04:07 +1200

    Remove parse & softobject modules Move assembler up a level

--- softobject/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-""" Javascripty objects """
-
-class SoftObject(object):
-
-    def __getattr__(self, item):
-        """ Makes object attributes soft """
-        return self.__dict__.setdefault(item, None)
